5a981f3ef08eeac76b33cf1328707004a39cecd2
Josh Gubler <josh.gubler@gmail.com>
2020-02-13 23:35:36 -0700

    fix buildspec

--- terraform-iac/dev/pipeline/pipeline.tf
+++ terraform-iac/dev/pipeline/pipeline.tf
@@ -27,7 +27,7 @@ module "buildspec" {
   source        = "github.com/byu-oit/terraform-aws-basic-codebuild-helper?ref=v0.0.2"
   ecr_repo_name = "hello-world-docker-api-dev"
   artifacts     = ["./terraform-iac/dev/app/*"]
-  pre_script    = ["pushd src", "npm install", "popd"]
+  pre_script    = ["cd src", "npm install", "cd .."]
 }
 
 module "my_codepipeline" {
