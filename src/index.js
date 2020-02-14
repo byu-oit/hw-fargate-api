3e7a94393d19a9cea8ba2750ce8234c49b3eb087
Josh Gubler <josh.gubler@gmail.com>
2020-02-14 00:28:59 -0700

    update api code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB({region: "us-west-2"});
 
 app.use((req, res, next) => {
   console.log(`${req.method} called on ${req.path} on ${new Date().toISOString()}`)
@@ -12,10 +14,23 @@ app.get('/health', (req, res) => {
 
 app.get('/', (req, res) => {
 
-  res.send(JSON.stringify({
-    secret: process.env.SOME_SECRET,
-    table: process.env.DYNAMO_TABLE_NAME
-  }))
+  var params = {
+    TableName: process.env.DYNAMO_TABLE_NAME
+  };
+  dynamodb.describeTable(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.status(500)
+      res.send("Error reading table")
+    }
+    else {
+      res.send(JSON.stringify({
+        secret: process.env.SOME_SECRET,
+        table: process.env.DYNAMO_TABLE_NAME,
+        numItems: data.Table.ItemCount
+      }))
+    }
+  });
 })
 
 app.listen(8080, () => {
