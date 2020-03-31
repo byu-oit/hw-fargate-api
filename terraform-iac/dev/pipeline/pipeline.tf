571542beb2c8b4ded6ed4b2dd3747b381493c3ce
Josh Gubler <josh.gubler@gmail.com>
2020-03-31 09:57:03 -0600

    fix file location

--- terraform-iac/dev/pipeline/pipeline.tf
+++ terraform-iac/dev/pipeline/pipeline.tf
@@ -12,39 +12,12 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-module "acs" {
-  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v2.0.0"
-}
-
 provider "github" {
   organization = "byu-oit"
   token        = module.acs.github_token
 }
 
-module "my_codepipeline" {
-  source                        = "github.com/byu-oit/terraform-aws-fargate-codepipeline?ref=v0.1.0"
-  pipeline_name                 = "hello-world-api-dev"
-  role_permissions_boundary_arn = module.acs.role_permissions_boundary.arn
-  power_builder_role_arn        = module.acs.power_builder_role.arn
-
-  required_tags = {
-    env              = "dev"
-    data-sensitivity = "public"
-  }
-
-  //Source
-  source_github_owner  = "byu-oit"
-  source_github_repo   = "hello-world-api"
-  source_github_branch = "dev"
-  source_github_token  = module.acs.github_token
-
-  //Build
-  # use buildspec.yml from source (default)
-
-  //Deploy
-  deploy_terraform_application_path = "./terraform-iac/dev/app/"
-  deploy_code_deploy_config = {
-    ApplicationName     = "hello-world-api-dev-codedeploy"
-    DeploymentGroupName = "hello-world-api-dev-deployment-group"
-  }
+module "pipeline" {
+  source = "../../modules/pipeline/"
+  env    = "dev"
 }
