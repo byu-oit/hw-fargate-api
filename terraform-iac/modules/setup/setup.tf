63809e95953d5b71c8c0fb2fc22819ac3c95f4fc
Josh Gubler <josh.gubler@gmail.com>
2020-10-23 16:08:14 -0600

    test least privilege

--- terraform-iac/modules/setup/setup.tf
+++ terraform-iac/modules/setup/setup.tf
@@ -20,3 +20,30 @@ module "my_ecr" {
   source = "github.com/byu-oit/terraform-aws-ecr?ref=v1.1.1"
   name   = "${local.name}-${var.env}"
 }
+
+data "aws_iam_policy" "power_user" {
+  name = "PowerUserPolicy"
+}
+
+data "aws_iam_user" "github_actions" {
+  user_name = "GitHub-Actions"
+}
+
+resource "aws_iam_role" "deploy" {
+  name = "${local.name}-deploy"
+  permissions_boundary = data.aws_iam_policy.power_user.arn
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowIamUserAssumeRole",
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Principal": {"AWS": "${data.aws_iam_user.github_actions.arn}"},
+        }
+    ]
+}
+EOF
+}
+
