ff423c362e286b400a11120fce0f69a0353a829d
GaryGSC <GaryGSC@users.noreply.github.com>
2021-06-15 13:14:52 -0600

    Revert "Intentionally introduce test & lint failure"
    This reverts commit 643c2486

--- src/app.test.js
+++ src/app.test.js
@@ -4,7 +4,6 @@ const app = require('./app')
 
 describe('GET /health', () => {
   test('should return 200', async () => {
-    throw Error();
     const response = await request(app).get('/health')
     expect(response.statusCode).toBe(200)
   })
