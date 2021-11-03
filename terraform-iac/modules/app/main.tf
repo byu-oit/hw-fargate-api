dd5ede505e6e54a7534628564afa7c09be235eba
GaryGSC <GaryGSC@users.noreply.github.com>
2021-11-03 11:07:02 -0600

    Bump Terraform versions

--- terraform-iac/modules/app/main.tf
+++ terraform-iac/modules/app/main.tf
@@ -32,7 +32,7 @@ data "aws_ecr_repository" "my_ecr_repo" {
 }
 
 module "acs" {
-  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v3.2.0"
+  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v3.4.0"
 }
 
 module "my_fargate_api" {
--- terraform-iac/stg/app/main.tf
+++ terraform-iac/stg/app/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.0.0"
+  required_version = "1.0.10"
   backend "s3" {
     bucket         = "terraform-state-storage-977306314792"
     dynamodb_table = "terraform-state-lock-977306314792"
